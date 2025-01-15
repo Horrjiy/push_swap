087baab8edcc3645370eb6b48a029c7134b4505e
Mischa Poplow <horrikohl.dx@gmail.com>
2025-01-15 12:49:55 +0100

    tried more stuff with the linked list

--- src/_main_push_swap.c
+++ src/_main_push_swap.c
@@ -6,7 +6,7 @@
 /*   By: mpoplow <mpoplow@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 17:41:19 by mpoplow           #+#    #+#             */
-/*   Updated: 2025/01/13 16:44:53 by mpoplow          ###   ########.fr       */
+/*   Updated: 2025/01/15 12:49:12 by mpoplow          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,32 @@
 
 int	main(int argc, char *argv[])
 {
-	t_list	*head;
+	t_list	*list;
+	t_list	*temp;
 	t_data	data;
 	int		i;
-	t_list	*node;
 
 	ft_arg_convert(argc, argv, &data);
-	i = 0;
+	list = ft_init_node(0, &data, list);
+	i = 1;
 	while (i < (argc - 1))
 	{
+		list->next = ft_init_node(i, &data, list);
+		temp = list;
+		list = list->next;
+		list->back = temp;
+		i++;
 	}
 }
 
-t_list	*ft_init_node(int i, t_data *data, t_list *head)
+t_list	*ft_init_node(int i, t_data *data, t_list *list)
 {
-	t_list *node;
+	t_list	*node;
+
 	node = (t_list *)malloc(sizeof(t_list));
 	if (!node)
-		ft_error(&data, head);
+		ft_error(&data, list);
 	node->num = data->arglist[i];
 	node->next = NULL;
 	node->back = NULL;
-		i++;
 }
--- src/list_ft.c
+++ src/list_ft.c
@@ -1,18 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   initstacks.c                                       :+:      :+:    :+:   */
+/*   list_ft.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mpoplow <mpoplow@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 17:31:58 by mpoplow           #+#    #+#             */
-/*   Updated: 2025/01/13 16:24:57 by mpoplow          ###   ########.fr       */
+/*   Updated: 2025/01/15 12:22:24 by mpoplow          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-// t_list	*ft_init_a(int i, t_data *data, t_list *a)
-// {
-	
-// }
+t_list	*ft_lastnode(t_list *list)
+{
+	t_list	*temp;
+
+	temp = list;
+	if (!temp)
+		return (NULL);
+	while (temp->next != NULL)
+		temp = temp->next;
+	return (temp);
+}
